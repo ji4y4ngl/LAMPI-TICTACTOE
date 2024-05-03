84bd65f34d2ef7a2dd8a5a45b78779cf4be1c17c
endarli <endar.li215@gmail.com>
2024-05-03 12:22:20 -0400

    edit app

--- lampi/lampi_app.py
+++ lampi/lampi_app.py
@@ -290,12 +290,12 @@ class GameScreen(Screen):
             
         for row in range(3):
             for col in range(3):
-                btn_id = f"btn{row * 3 + col}"
+                btn_id = f"btn{row * 3 + col + 1}"
                 print(f"board_string @ {row * 3 + col}: {board_string[row * 3 + col]}")
-                if int(board_string[row * 3 + col + 1]) == 1:
+                if int(board_string[row * 3 + col]) == 1:
                     self.ids[btn_id].text = "X"
                     print(f"updated: {self.ids[btn_id]} to X")
-                if int(board_string[row * 3 + col + 1]) == 2:
+                if int(board_string[row * 3 + col]) == 2:
                     self.ids[btn_id].text = "O"
                     print(f"updated: {self.ids[btn_id]} to O")
 
